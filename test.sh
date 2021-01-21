53ebb1c2f4f74b4f0f8ad4312e2f982a897e692b
fengpan0403 <fengpan0403@163.com>
2021-01-21 19:06:40 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Diagnostic output:
+echo "Using options: $INPUT_OPTIONS"
+echo '============================='
+echo
+
+# Runs misspell-fixes:
+output=$(/misspell-fixer/misspell-fixer $INPUT_OPTIONS)
+status="$?"
+
+# Sets the output variable for GitHub Action API:
+# See: https://help.github.com/en/articles/development-tools-for-github-action
+echo "::set-output name=output::$output"
+echo '================================='
+echo
+
+# Fail the build in case status code is not 0:
+if [ "$status" -ge 100 ]; then
+  echo 'Failing with output:'
+  echo "$output"
+  echo "Process failed with the status code: $status"
+  exit "$status"
+fi
+
+# This is a special comment, please do not touch: we use it for tests.
+# test: lower then
